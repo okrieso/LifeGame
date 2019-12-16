271d3fe6f6e18df42af26980ba95693250456293
Bomjick <senichevleonid@yandex.ru>
2019-12-16 17:06:16 +0300

    Add Function HowMuch

--- LifeGame.pas
+++ LifeGame.pas
@@ -1,4 +1,21 @@
-﻿var A : array [,] of boolean;
+﻿var
+  A: array [,] of boolean;
+
+function HowMuch(x, y: integer): integer;
+begin
+  result := 0;
+  for var i := -1 to 1 do
+    for var j := -1 to 1 do
+      if (i <> j) and (A[x + i, y + j]) then
+        result+=1;
+    
+end;
+   
+      
+  
 
 begin
+  
+  
+  
 end.
