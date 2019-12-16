a63ee1b7a6df0d171f0371dad124acbda608dc10
okrieso <okrieso@gmail.com>
2019-12-16 17:26:12 +0300

    minor fixes

--- LifeGame.pas
+++ LifeGame.pas
@@ -1,14 +1,17 @@
-var A : array [,] of boolean;
+﻿var
+  A: array [,] of boolean;
 
 procedure PrintField();
 begin
-  for var i := 1 to A.RowCount-2 do
+  for var i := 1 to A.RowCount - 2 do
   begin
-    for var j := 1 to A.ColCount-2 do
-      if a[i,j] then Print('█֯֯')
+    for var j := 1 to A.ColCount - 2 do
+      if a[i, j] then Print('█֯֯')
       else Print('░');
     writeln();
   end;
+  writeln();
+  writeln();
 end;
 
 function HowMuch(x, y: integer): integer;
@@ -17,39 +20,39 @@ begin
   for var i := -1 to 1 do
     for var j := -1 to 1 do
       if (i <> j) and (A[x + i, y + j]) then
-        result+=1;
+        result += 1;
 end;
 
 procedure OneIteration();
-  begin
-    for var i:=1 to A.rowCount-2 do
-      for var j:=1 to A.ColCount-2 do
-        if A[i,j]=true then
-          if (HowMuch(i,j) < 2) or (HowMuch(i,j) > 3) then
-            A[i,j]:=false
-        else
-          if (HowMuch(i,j) = 3) then
-            A[i,j]:=true;   
-  end;
- 
 begin
-  A:=matrRandom(5,5);
-  for var i:=0 to a.GetLength(0) - 1 do
+  for var i := 1 to A.rowCount - 2 do
+    for var j := 1 to A.ColCount - 2 do
+      if A[i, j] = true then
+        if (HowMuch(i, j) < 2) or (HowMuch(i, j) > 3) then
+          A[i, j] := false
+        else
+        if (HowMuch(i, j) = 3) then
+          A[i, j] := true;   
+end;
+
+begin
+  A := MatrGen(10, 10, (x, y)-> Random(2) = 0);
+  for var i := 0 to a.GetLength(0) - 1 do
   begin
-    a(i,0):= false;
-    a(i,a.GetLength(1)-1):= false;
+    a[i, 0] := false;
+    a[i, a.GetLength(1) - 1] := false;
   end;
-  for var i:=0 to a.GetLength(1) - 1 do
+  for var i := 0 to a.GetLength(1) - 1 do
   begin
-    a(0,i):= false;
-    a(a.GetLength(0)-1, i):= false;
+    a[0, i] := false;
+    a[a.GetLength(0) - 1, i] := false;
   end;
   printfield;
-  while A.any(x -> x) then
+  while true do
   begin
     OneIteration;
     printfield;
     sleep(100);
   end;
-
+  
 end.
