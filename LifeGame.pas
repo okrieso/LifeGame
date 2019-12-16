7b6801d0b552bfc6ee2fdc854299b4f3802eea66
Moryasynn <andsimof@gmail.com>
2019-12-16 16:48:19 +0300

    Added PrintField() func that PRINTS FIELD

--- LifeGame.pas
+++ LifeGame.pas
@@ -1,4 +1,16 @@
 ﻿var A : array [,] of boolean;
 
+procedure PrintField();
 begin
+  for var i := 0 to A.RowCount-1 do
+  begin
+    for var j := 0 to A.ColCount-1 do
+      if a[i,j] then Print('█֯֯')
+      else Print('░');
+    writeln();
+  end;
+end;
+
+begin
+  
 end.
