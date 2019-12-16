bac0003426c2a8c99826eefe77cbd73294e24b2a
Murrr8 <yanulik01@mail.ru>
2019-12-16 17:06:05 +0300

    Add procedure one-iteration

--- LifeGame.pas
+++ LifeGame.pas
@@ -1,5 +1,19 @@
 ﻿
+var A : array [,] of boolean;
+
+procedure OneIteration();
+  begin
+    for var i:=1 to A.rowCount-2 do
+      for var j:=1 to A.ColCount-2 do
+        if A[i,j]=true then
+          if (HowMuch(i,j) < 2) or (HowMuch(i,j) > 3) then
+            A[i,j]:=false
+        else
+          if (HowMuch(i,j) = 3) then
+            A[i,j]:=true;   
+  end;
+  
 
 begin
-  
+
 end.
