0f133449b3fe85573bd68f902bee4fbd8ee3e2da
okrieso <okrieso@gmail.com>
2019-12-16 17:13:27 +0300

    add Main Cycle

--- LifeGame.pas
+++ LifeGame.pas
@@ -1,4 +1,22 @@
 ﻿var A : array [,] of boolean;
 
 begin
+  A:=matrRandom(5,5);
+  for var i:=0 to a.GetLength(0) - 1 do
+  begin
+    a(i,0):= false;
+    a(i,a.GetLength(1)-1):= false;
+  end;
+  for var i:=0 to a.GetLength(1) - 1 do
+  begin
+    a(0,i):= false;
+    a(a.GetLength(0)-1, i):= false;
+  end;
+  printfield;
+  while A.any(x -> x) then
+  begin
+    OneIteration;
+    printfield;
+    sleep(100);
+  end;
 end.
